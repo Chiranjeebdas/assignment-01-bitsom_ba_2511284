8cb223384a366630f11096db18e7a32faf32506b
Chiranjeebdas <chiranjeebd8@gmail.com>
2026-03-23 21:55:18 +0530

    Update queries.sql

--- part1-rdms/queries.sql
+++ part1-rdms/queries.sql
@@ -1,1 +1,14 @@
+-- Q1
+SELECT customer_name FROM customers WHERE customer_city='Mumbai';
 
+-- Q2
+SELECT product_name FROM products LIMIT 3;
+
+-- Q3
+SELECT sales_rep_name FROM sales_reps;
+
+-- Q4
+SELECT order_id FROM orders;
+
+-- Q5
+SELECT product_name FROM products;
