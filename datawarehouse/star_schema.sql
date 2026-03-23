fdbebaa1ce19518b74388daec009e22836bf6e42
Chiranjeebdas <chiranjeebd8@gmail.com>
2026-03-23 22:18:47 +0530

    Update star_schema.sql

--- datawarehouse/star_schema.sql
+++ datawarehouse/star_schema.sql
@@ -34,3 +34,31 @@ CREATE TABLE fact_sales (
     FOREIGN KEY (store_id) REFERENCES dim_store(store_id),
     FOREIGN KEY (product_id) REFERENCES dim_product(product_id)
 );
+
+
+
+
+
+
+
+-- INSERT DATA
+
+INSERT INTO dim_store VALUES
+(1, 'Chennai Anna', 'Chennai'),
+(2, 'Delhi South', 'Delhi'),
+(3, 'Bangalore MG', 'Bangalore');
+
+INSERT INTO dim_product VALUES
+(1, 'Speaker', 'Electronics'),
+(2, 'Jeans', 'Clothing'),
+(3, 'Atta 10kg', 'Grocery');
+
+INSERT INTO dim_date VALUES
+(1, '2023-08-29', 8, 2023),
+(2, '2023-12-12', 12, 2023),
+(3, '2023-02-05', 2, 2023);
+
+INSERT INTO fact_sales VALUES
+(1, 1, 1, 1, 3, 49262.78, 147788.34),
+(2, 2, 1, 1, 11, 23226.12, 255487.32),
+(3, 3, 1, 1, 20, 48703.39, 974067.80);
