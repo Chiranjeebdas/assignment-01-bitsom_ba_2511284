61c1bca9e91a9a00215243733c76d579317528ab
Chiranjeebdas <chiranjeebd8@gmail.com>
2026-03-23 21:54:18 +0530

    Update schema_design.sql

--- part1-rdms/schema_design.sql
+++ part1-rdms/schema_design.sql
@@ -1,1 +1,42 @@
+CREATE TABLE customers (
+    customer_id VARCHAR(10) PRIMARY KEY,
+    customer_name VARCHAR(100),
+    customer_email VARCHAR(100),
+    customer_city VARCHAR(50)
+);
 
+CREATE TABLE products (
+    product_id VARCHAR(10) PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    unit_price INT
+);
+
+CREATE TABLE sales_reps (
+    sales_rep_id VARCHAR(10) PRIMARY KEY,
+    sales_rep_name VARCHAR(100),
+    sales_rep_email VARCHAR(100),
+    office_address TEXT
+);
+
+CREATE TABLE orders (
+    order_id VARCHAR(10) PRIMARY KEY,
+    customer_id VARCHAR(10),
+    sales_rep_id VARCHAR(10),
+    order_date DATE
+);
+
+CREATE TABLE order_items (
+    order_item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id VARCHAR(10),
+    product_id VARCHAR(10),
+    quantity INT,
+    total_amount INT
+);
+
+INSERT INTO customers VALUES
+('C001','Rohan Mehta','rohan@gmail.com','Mumbai'),
+('C002','Priya Sharma','priya@gmail.com','Delhi'),
+('C003','Amit Verma','amit@gmail.com','Bangalore'),
+('C004','Sneha Iyer','sneha@gmail.com','Chennai'),
+('C005','Vikram Singh','vikram@gmail.com','Mumbai');
