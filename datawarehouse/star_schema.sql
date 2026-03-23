ea890804c700a672934d557563b72e77f1e5da20
Chiranjeebdas <chiranjeebd8@gmail.com>
2026-03-23 22:18:02 +0530

    Update star_schema.sql

--- datawarehouse/star_schema.sql
+++ datawarehouse/star_schema.sql
@@ -1,1 +1,36 @@
+-- DIMENSION TABLES
 
+CREATE TABLE dim_date (
+    date_id INT PRIMARY KEY,
+    full_date DATE,
+    month INT,
+    year INT
+);
+
+CREATE TABLE dim_store (
+    store_id INT PRIMARY KEY,
+    store_name VARCHAR(100),
+    store_city VARCHAR(100)
+);
+
+CREATE TABLE dim_product (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50)
+);
+
+-- FACT TABLE
+
+CREATE TABLE fact_sales (
+    sales_id INT PRIMARY KEY,
+    date_id INT,
+    store_id INT,
+    product_id INT,
+    units_sold INT,
+    unit_price DECIMAL(10,2),
+    total_amount DECIMAL(10,2),
+
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (store_id) REFERENCES dim_store(store_id),
+    FOREIGN KEY (product_id) REFERENCES dim_product(product_id)
+);
